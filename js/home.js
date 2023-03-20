18227fc669a9289c3138467f830a338501b74f18
Ayushchauhan90 <122205482+Ayushchauhan90@users.noreply.github.com>
2023-03-20 19:52:39 +0530

    Add files via upload

---
+++ js/home.js
@@ -0,0 +1,32 @@
+$('.add-slide-image').owlCarousel({
+    loop:true,
+    autoplay:true,
+    // autoplayTimeout: 1000,
+    autoplayHoverPause:true,
+    margin:10,
+    nav:true,
+    responsive:{
+        0:{
+            items:1.6
+        },
+        600:{
+            items:2
+        },
+        800:{
+            items:1.2
+        }
+    }
+})
+// $(document).ready(function(){
+//     $(".menu-img").on("click",function(){
+//         $(this).removeClass("inner-text")
+//         $(this).hide()
+//     })
+// })
+
+
+
+
+
+
+
